a3e473eae08e143c793c0719ec831dd11f697508
lixiaogang <lixg1993@qq.com>
2018-10-14 10:02:55 +0800

    1

---
+++ 代码/chapter3/demo/code/3-4_correlation_analyze.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#餐饮销量数据相关性分析
+from __future__ import print_function
+import pandas as pd
+
+catering_sale = '../data/catering_sale_all.xls' #餐饮数据，含有其他属性
+data = pd.read_excel(catering_sale, index_col = u'日期') #读取数据，指定“日期”列为索引列
+
+#相关系数矩阵，即给出了任意两款菜式之间的相关系数.
+print('任意两款菜品间相关系数:\n',data.corr(),'\n') 
+
+#只显示“百合酱蒸凤爪”与其他菜式的相关系数
+print('“百合酱蒸凤爪”与其他菜式的相关系数:\n',data.corr()[u'百合酱蒸凤爪'],'\n')
+
+#计算“百合酱蒸凤爪”与“翡翠蒸香茜饺”的相关系数
+print('“百合酱蒸凤爪”与“翡翠蒸香茜饺”的相关系数:\n',data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺'])) 
